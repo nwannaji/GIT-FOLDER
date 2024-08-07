cdd1329ad5bea6b77ea81302a1cefe7aa221aaca
Henrio <edenwannaji1980@gmail.com>
2024-08-07 22:44:38 +0100

    Improve the Employee model for better user experience

--- core/vms/models.py
+++ core/vms/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Employee(models.Model):
     employee_Id = models.IntegerField(primary_key=True,blank=False,null=False, unique=True)
     employee_name = models.CharField(max_length=200,blank=False,null=False, verbose_name='Employee Name')
-    dept_name = models.CharField(max_length=100,blank=False,null=False)
+    dept_name = models.CharField(max_length=100,blank=False,null=False, verbose_name='Department Name')
     unit = models.CharField(max_length=100, blank=True, null=True)
-    date_of_employment = models.DateTimeField(blank=True,null=True)
-    email_address = models.CharField(max_length=150,unique=True,blank=False,null=False, help_text='Please use your official email address.')
+    date_of_employment = models.DateTimeField(blank=True,null=True, verbose_name='Date of Emplyment')
+    email_address = models.CharField(max_length=150,unique=True,blank=False,null=False, verbose_name='Email Address', help_text='Please use your official email address.')
     phone_number = models.CharField(max_length=15,unique=True,blank=False, null=False, help_text='Please type your mobile number.')
-    is_Admin = models.BooleanField(default=False)
-    is_contract_staff = models.BooleanField(default=False, blank=True, null=True, verbose_name='Contract staff')
+    is_Admin = models.BooleanField(default=False, verbose_name='Is Admin?')
+    is_contract_staff = models.BooleanField(default=False, blank=True, null=True, verbose_name='Contract staff ?')
 
     def __str__(self) -> str:
         return self.employee_name
