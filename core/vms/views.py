27f76157275bd510fdab1092e24558aaeab3028a
Henrio <edenwannaji1980@gmail.com>
2024-08-09 14:59:43 +0100

    I fixed the issue with not been able to programatically assign value to the model field 'whom_to_see',before now, but that has been resolve. I also refactor the default admin page to ensure everything sync together

--- core/vms/views.py
+++ core/vms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from vms.models import Visitor
+from vms.models import Employee
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('About to implement front-end')
+
+
+
+
